297af4dff52e8e79056b99d32991670f5f56caf3
Elizabeth Hudnott <lizzy.hudnott@gmail.com>
2016-02-11 16:27:56 +0000

    Initial content

---
+++ js/navigation.js
@@ -0,0 +1,35 @@
+function hideMenus() {
+	$('.menu-items').removeClass("menu-open");	
+}
+
+function showMenu(target) {
+	hideMenus();
+	var items = $(target).children(".menu-items");
+	items.addClass("menu-open");
+	
+}
+
+function toggleMenu(target) {
+	var elemJQ = $(target);
+	var menuIsOpen = elemJQ.children(".menu-items").hasClass("menu-open");
+	hideMenus();
+	if (!menuIsOpen) {
+		elemJQ.children(".menu-items").addClass("menu-open");
+	}
+}
+
+function initialize() {
+	var menus = $('.menu');
+	
+	menus.on("click", function () {
+		toggleMenu(this);
+	});
+	
+	menus.on("mouseenter", function () {
+		showMenu(this);
+	});
+	
+	menus.on("mouseleave", function () {
+		hideMenus();
+	});
+}
